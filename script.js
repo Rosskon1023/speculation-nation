076204a8751fdf9c9190a752b264602f8fd3f07d
rkonesky <rosskon1023@gmail.com>
2021-10-28 17:24:50 -0500

    wip: Finishing crypto card display template

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
-// alert('JS is working!');
+// Constants - Data that does not Change
 
-// $header = $('h1');
-// console.log($header);
+// Variables - Data that Changes 
+
+// Cached Elements
+
+// Event Listners 
+
+// Functions
+
+// console.log($.ajax('https://min-api.cryptocompare.com/data/pricemultifull?fsyms=ETH,BTC&tsyms=USD&api_key=126a3db639a3dc303efb721f6f93919d579bc1edd1b38726c4c4f01c16933771'));
+// .then(function (data) {
+    //     console.log(data);
+    // }, function (error) {
+    //     console.log('promise failed')
+    //     console.log(error);
+    // });
+
+// 126a3db639a3dc303efb721f6f93919d579bc1edd1b38726c4c4f01c16933771
+
+
+
+// https://www.cryptocompare.com/media/37746242/link.png
